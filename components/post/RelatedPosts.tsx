dfb626e3d3ca763ffaaf1e9ef3aad3a32f136063
urke90 <urosbijelic90@gmail.com>
2024-04-04 16:03:03 +0200

    added comments to RelatedPosts.tsx

--- components/post/RelatedPosts.tsx
+++ components/post/RelatedPosts.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import { Button } from '../ui/button';
 
-type IRelatedPostsProps = {};
+interface IRelatedPostsProps {}
 
 /**
  * TODO  Check later if list item have onClick handler and what would the flow be????
